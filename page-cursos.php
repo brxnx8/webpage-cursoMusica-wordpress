4f8aa1f86dcaa9c586445bcf5ae5b600634b42c4
Felipe Engel <felipesepulvida@id.uff.br>
2022-09-09 14:11:03 -0300

    feat(page-cursos): Adicionadas as tags scripts para a animação das seções usando Js

--- page-cursos.php
+++ page-cursos.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name') ?> | <?php the_title(); ?></title>
     <?php wp_head(); ?>
+    <script src="<?= get_template_directory_uri()?>/js/addCourseSectionAnimation.js" type="text/javascript"></script>
 </head>
 <body>
 
@@ -21,14 +22,18 @@
 
 <div class="coursesOptions">
     <?php 
-        foreach($courses as $post){
+        foreach($courses as $courseIndex => $post){
             setup_postdata($post);
-            createCourseSection();
+            createCourseSection($courseIndex);
         }
     ?>
 </div> 
 
 
+
+<script type="text/javascript">
+    addAnimationToCourses(<?= sizeof($courses) ?>, 0.4)
+</script>
 
 </body>
 </html>
