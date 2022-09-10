62054f6b4a521f9284208e940e36465b03e5740c
Bruno Oliveira <brunogabriel2608@gmail.com>
2022-09-09 23:55:58 -0300

    <FEAT>: Terminado footer /  passando estilizaçao da secao sobre e do footer para documento style.css.

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php wp_footer(); ?>
+    <footer class="footer">
+        <section class="informations">
+            <div class="informations-footer">
+                <p class="p-footer"><img src="assets/images/instagramiconfooter.png" width="40px" alt="" class="icone-footer"> @curso-music</p>
+                <p class="p-footer"><img src="assets/images/whatsappiconfooter.png" width="40px" alt="" class="icone-footer"> @curso-music</p>
+                <p class="p-footer"><img src="assets/images/e-mailiconfooter.png" width="40px" alt="" class="icone-footer"> @curso-music</p>
+            </div>
+            <div class="logo-footer">
+                <img src="assets/images/Logo.png" width="120px" height="110px" alt="" >
+                <p>CNPJ: 00000/0001</p>
+            </div>
+        </section>
+        <section class="copyright">
+            <div class="copyright-footer">copyright PROJETO 2022 - todos os direitos reservados</div>
+        </section>
+    </footer>
+</body>
+</html>
