fbc0de87d40a475f9e7df829ca7c18d8564afc01
Bruno Oliveira <brunogabriel2608@gmail.com>
2022-09-10 17:59:58 -0300

    <FEAT>: adicionado funcoes php ao footer para ele mostrar imagens

--- footer.php
+++ footer.php
@@ -2,12 +2,12 @@
     <footer class="footer">
         <section class="informations">
             <div class="informations-footer">
-                <p class="p-footer"><img src="assets/images/instagramiconfooter.png" width="40px" alt="" class="icone-footer"> @curso-music</p>
-                <p class="p-footer"><img src="assets/images/whatsappiconfooter.png" width="40px" alt="" class="icone-footer"> @curso-music</p>
-                <p class="p-footer"><img src="assets/images/e-mailiconfooter.png" width="40px" alt="" class="icone-footer"> @curso-music</p>
+                <p class="p-footer"><img src="<?= get_template_directory_uri() ?>/assets/images/instagramiconfooter.png" width="30px" alt="" class="icone-footer"> @curso-music</p>
+                <p class="p-footer"><img src="<?= get_template_directory_uri() ?>/assets/images/whatsappiconfooter.png" width="30px" alt="" class="icone-footer"> @curso-music</p>
+                <p class="p-footer"><img src="<?= get_template_directory_uri() ?>/assets/images/e-mailiconfooter.png" width="30px" alt="" class="icone-footer"> @curso-music</p>
             </div>
             <div class="logo-footer">
-                <img src="assets/images/Logo.png" width="120px" height="110px" alt="" >
+                <img src="<?= get_template_directory_uri() ?>/assets/images/Logo.png" width="90px" height="70px" alt="" >
                 <p>CNPJ: 00000/0001</p>
             </div>
         </section>
